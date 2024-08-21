eb0c1990ddc53a3e6295e452f02869a41aeb5499
VioletaLopez <2708425860101@ingenieria.usac.edu.gt>
2024-08-21 17:42:18 -0600

    [UPDATE] Se agregaron componentes visuales a login

--- FrontEnd/NoteCraft/src/App.jsx
+++ FrontEnd/NoteCraft/src/App.jsx
@@ -1,12 +1,15 @@
 
 //local imports
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Profile from './pages/Profile'
 
 function App() {
 
   return (
     <>
-      <Home />
+      <Login />
     </>
   )
 }
