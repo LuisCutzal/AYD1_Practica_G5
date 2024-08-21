6db5f72edb5b7d4f207993ff16f15942d118e3ed
VioletaLopez <2708425860101@ingenieria.usac.edu.gt>
2024-08-21 17:42:18 -0600

    [UPDATE] Se agregaron componentes visuales a login

--- FrontEnd/NoteCraft/tailwind.config.js
+++ FrontEnd/NoteCraft/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Marker': ['"Permanent Marker"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
