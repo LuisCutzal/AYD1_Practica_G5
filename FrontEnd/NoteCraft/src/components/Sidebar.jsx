e9107f1b9a849494fc5f6c2bcc34db2757a54c95
Pedro <2574151141308@ingenieria.usac.edu.gt>
2024-08-22 01:40:23 -0600

    [UPDATE] Mostrat notas archivas

--- FrontEnd/NoteCraft/src/components/Sidebar.jsx
+++ FrontEnd/NoteCraft/src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import propTypes from "prop-types";
 //local imports
 import NoteForm from "./NoteForm";
 
-function Sidebar({ isLoggedIn }) {
+function Sidebar({ isLoggedIn, onShowArchivedNotes }) {
   const [isModalOpen, setModalOpen] = useState(false);
   const [tags, setTags] = useState(['Trabajo', 'Personal']);
   const [notes, setNotes] = useState([]);
@@ -64,7 +64,7 @@ function Sidebar({ isLoggedIn }) {
               <path d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
             </svg>
             <a href="#" className="text-gray-700 hover:text-blue-500"
-              onClick={isLoggedIn ? null : (e) => e.preventDefault()}>
+              onClick={isLoggedIn ? onShowArchivedNotes : (e) => e.preventDefault()}>
               Archivados
             </a>
           </li>
@@ -76,6 +76,7 @@ function Sidebar({ isLoggedIn }) {
 
 Sidebar.propTypes = {
   isLoggedIn: propTypes.bool.isRequired,
+  onShowArchivedNotes: propTypes.func.isRequired
 };
 
 export default Sidebar;
